e890df3a3572bd6b4ead07d14d2fae5435fa8114
Thejus Sunny <thejusunny1@gmail.com>
2026-08-20 13:00:25 +0400

    API versioning added on top of routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 //Routes
-const authRoutes = require('./routes/authRoutes')
+const apiRoutes = require('./routes/index')
 app.use(express.json())
 
-app.use('/auth',authRoutes)
+app.use('/v1',apiRoutes)
 
 
 module.exports = app
