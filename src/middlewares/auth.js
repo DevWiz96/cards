e625f7638719f8871ea780470176ea2d78a6bcbb
DevWiz96 <devwiz96@gmail.com>
2026-09-07 12:33:15 +0400

    Update auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,7 +4,7 @@ module.exports = (req,res,next)=>{
 
     const token = req.cookies.token
     if(!token)
-        res.status(401).json({error:"No token was available"})
+        return res.status(401).json({error:"No token was available"})
 
     try
     {
